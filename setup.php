5ef22c4203554eeccff1a16c00ea3d9f9de1adb6
Ieva Šinke <ieva.sinke@gmail.com>
2024-07-03 13:25:44 +0300

    db schema

---
+++ setup.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Medoo\Medoo;
+
+$database = new Medoo([
+    'type' => 'sqlite',
+    'database' => 'storage/database.sqlite'
+]);
+
+$database->query("CREATE TABLE IF NOT EXISTS articles (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+author VARCHAR(255) NOT NULL,
+title TEXT NOT NULL,
+content TEXT NOT NULL,
+created_at DATETIME NOT NULL,
+updated_at DATETIME NOT NULL,
+deleted_at DATETIME NOT NULL
+)");
+
+echo "Database schema initialized.\n";
